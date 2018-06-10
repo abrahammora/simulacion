bb72ebb34356f1c1a0c83abe8f9a1a1f3a0ba9d0
Abraham <33945845+abrahammora@users.noreply.github.com>
2018-06-10 12:15:58 -0500

    main v1.1
    Opcion de menu de archivos

--- CensoPuebla/src/Main.java
+++ CensoPuebla/src/Main.java
@@ -8,55 +8,78 @@ public class Main {
 		System.out.println("1.-Metodo de Lagrange");
 		System.out.println("2.-Diferencias Divididas");
 	}
+	public static String menuArchivos(String[] files) {
+		Scanner leer = new Scanner(System.in);
+		System.out.println("¿Que archivo quieres usar?");		
+		for (int i = 0; i < files.length; i++) {
+			if(files[i]!=null) {
+				System.out.println(i+1+".-"+files[i]);
+				
+			}
+								
+		}	
+		return leer.nextLine();
+	}
+	
+	
 	public static void main(String[] args) {
 		Scanner leer = new Scanner(System.in);
 		String opcion;
-		Interpolacion metodos;
-		double año[]= {1980,1990,2000,2010};
-		double poblacion[] = {817431,1054926,1489763,2384587};
-		double x2[]= {8.1,8.3,8.6,8.7};
-		double fx2[]= {16.94410,17.56492,18.50515,18.82091};
-		String valor1 = null,valor2;
-		String ruta = "C:\\Users\\abraham\\eclipse-workspace\\CensoPuebla\\src\\censo.txt";
-		Archivo archivo=null;
+		Interpolacion metodos;		
+		String ruta;				
+		File dire = new File("C:\\Users\\abraham\\eclipse-workspace\\CensoPuebla\\src");		
+		String[] ficheros = dire.list();
+		String[] ficherostxt=new String[ficheros.length];
+		int con=0;
+		for (int i = 0; i < ficheros.length; i++) {
+			if(ficheros[i].contains(".txt") && ficheros[i]!=null) {	
+				ficherostxt[con]=ficheros[i];
+				con++;
+			}				
+		}			
+		Archivo archivo=null;		
 		LinkedList<File> ListaArchivos = new LinkedList<>();
-		ListaArchivos.add(new File(ruta));
-		for (int i = 0; i < ListaArchivos.size(); i++) {
-			archivo = new Archivo(ListaArchivos.get(i));
-		}
-		String arr[] = archivo.getWords();
-		int n = Integer.parseInt(arr[0]);
-//		System.out.println(arr[arr.length-1]);
-		double x[] = new double[n+1];
-		double fx[] = new double[n+1];
-		String cadena="";		
-		int p=0,c;
-		for (int i = 0;i<arr.length; i++) {			
-			//cadena+=arr[i];
-			
-			for (int j = i+1; j<arr.length; j++) {
-				if(i%2==0 && j<arr.length-1&&p<=n){
-					//System.out.println(arr[i+1] +" : "+arr[j+1]);					
-					x[p]=Double.parseDouble(arr[i+1]);
-					fx[p]=Double.parseDouble(arr[j+1]);
-					p++;
-				}
-					
-				break;												
-				
-			}
-		//	p++;
-		}
-		metodos = new Interpolacion(x, fx); 
+	
+	
+		
+	
 		while(true) {
+			ruta = "C:\\Users\\abraham\\eclipse-workspace\\CensoPuebla\\src\\"+ficherostxt[Integer.parseInt(menuArchivos(ficherostxt))-1];
+			ListaArchivos.add(new File(ruta));
 			
+			for (int i = 0; i < ListaArchivos.size(); i++) {
+				archivo = new Archivo(ListaArchivos.get(i));
+			}
+			
+			String arr[] = archivo.getWords();
+			int n = Integer.parseInt(arr[0]);
+
+			double x[] = new double[n+1];
+			double fx[] = new double[n+1];
+			String cadena="";		
+			int p=0,c;
+			for (int i = 0;i<arr.length; i++) {			
+				//cadena+=arr[i];			
+				for (int j = i+1; j<arr.length; j++) {
+					if(i%2==0 && j<arr.length-1&&p<=n){
+						//System.out.println(arr[i+1] +" : "+arr[j+1]);					
+						x[p]=Double.parseDouble(arr[i+1]);
+						fx[p]=Double.parseDouble(arr[j+1]);
+						p++;
+					}
+						
+					break;												
+					
+				}
+			}
+			metodos = new Interpolacion(x, fx); 
 			menu();
 			opcion = leer.nextLine();
-			if(Integer.parseInt(opcion)==1) {
-
-				System.out.println("El resultado del censo en el 2008 es: "+String.format("%.2f",metodos.ResultadoLagrange(n,Double.parseDouble(arr[arr.length-1]))));
-			}else if(Integer.parseInt(opcion)==2) {
-				System.out.println(String.format("%.6f",metodos.ResultadoDiferencias(n,Double.parseDouble(arr[arr.length-1]))));
+			if(Integer.parseInt(opcion)==1) {					
+				System.out.println("El resultado de f("+arr[arr.length-1]+") es: "+String.format("%.6f",metodos.ResultadoLagrange(n,Double.parseDouble(arr[arr.length-1]))));
+			}else if(Integer.parseInt(opcion)==2) {		
+				
+				System.out.println("El resultado de f("+arr[arr.length-1]+") es: "+String.format("%.6f",metodos.TablaDiferenciasD(n,Double.parseDouble(arr[arr.length-1]))));
 			}
 			
 		}
