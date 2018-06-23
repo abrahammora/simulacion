3b8b97fb30707fe4d69b5cf92688e3d0e2ba0907
Abraham <33945845+abrahammora@users.noreply.github.com>
2018-06-23 14:50:17 -0500

    Simpson 1.2
    Simpson 3/8

--- simpson/Main.java
+++ simpson/Main.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 class Main{
+				public static void ImprimirTabla(double[] x,double[] fx,int n,double res){
+								System.out.println("\tx\t\tfx");
+								for(int i=0;i<x.length;i++){
+												System.out.println("\tx"+i+" "+String.format("%.3f",x[i])+"\t"+String.format("%.3f",fx[i]));
+								}
+								System.out.println("El resultado evaluado en "+n+" puntos es: "+String.format("%.4f",res));
+				}
 				public static void main(String[] args){
 						Simpson obj = new Simpson();
 						Scanner leer = new Scanner(System.in);
@@ -8,7 +15,8 @@ class Main{
 					String opcion;
 					while(true){
 
-									System.out.println("¿Qué regla quieres usar?\n1.-Regla Simpson 1/3\n2.-Regla Simpson 3/8\n3.-Regla Compuesta Simpson 1/3");
+									System.out.println("¿Qué regla quieres usar?\n1.-Regla Simpson 1/3\n2.-Regla Simpson 3/8\n3.-Regla Compuesta Simpson 1/3\n4.-Regla Compuesta Simpson 3/8");
+									System.out.print("Elige una opción:");
 									opcion = leer.nextLine();
 									System.out.print("Ingresa el valor de x0: ");
 									x0 = leer.nextLine();
@@ -22,11 +30,8 @@ class Main{
 											obj.setx(aux);
 											double[] auxfx = obj.valorfx();
 											obj.setfx(auxfx);
-											System.out.println("\tx\tfx");
-											for(int i=0;i<auxfx.length;i++){
-												System.out.println("\t"+aux[i]+"\t"+auxfx[i]);
-											}
-											System.out.println("El resultado evaluado en 3 puntos es: "+obj.SimpsonTercio());
+											ImprimirTabla(obj.getx(),obj.getfx(),obj.getn(),obj.SimpsonTercio());
+
 													
 									}else if(Integer.parseInt(opcion)==2){
 											obj.setn(4);
@@ -36,30 +41,43 @@ class Main{
 											obj.setx(aux);
 											double[] auxfx = obj.valorfx();
 											obj.setfx(auxfx);
-											System.out.println("\tx\tfx");
-											for(int i=0;i<auxfx.length;i++){
-												System.out.println("\t"+aux[i]+"\t"+auxfx[i]);
-											}
-											System.out.println("El resultado evaluado en 4 puntos es: "+obj.SimpsonOctavo());
-									}else if(Integer.parseInt(opcion)==3){
+											ImprimirTabla(obj.getx(),obj.getfx(),obj.getn(),obj.SimpsonOctavo());
 
-											System.out.print("Ingresa la cantidad de puntos a evaluar: ");
-											String n = leer.nextLine();
+									}else if(Integer.parseInt(opcion)==3){
+											String n;
+										do{
+												System.out.print("Ingresa la cantidad de puntos a evaluar: ");
+												 n = leer.nextLine();
 											if(Integer.parseInt(n)%2==0){											
-												obj.setn(Integer.parseInt(n));
-												double[] aux = obj.valorx(Double.parseDouble(x0),Double.parseDouble(xn),Integer.parseInt(opcion));
-												aux[0] = Double.parseDouble(x0);
-												aux[aux.length-1] = Double.parseDouble(xn);
-												obj.setx(aux);
-												double[] auxfx = obj.valorfx();
-												obj.setfx(auxfx);
-												System.out.println("\tx\tfx");
-												for(int i=0;i<auxfx.length;i++){
-													System.out.println("\t"+aux[i]+"\t"+auxfx[i]);
-												}
-												System.out.println("El resultado evaluado en "+n+" puntos es: "+obj.SimpsonCompuesta());
-										}else
+													obj.setn(Integer.parseInt(n)+1);
+													double[] aux=obj.valorx(Double.parseDouble(x0),Double.parseDouble(xn),Integer.parseInt(opcion));
+													aux[0] = Double.parseDouble(x0);
+													aux[aux.length-1] = Double.parseDouble(xn);
+													obj.setx(aux);
+													double[] auxfx = obj.valorfx();
+													obj.setfx(auxfx);
+													ImprimirTabla(obj.getx(),obj.getfx(),obj.getn()-1,obj.SimpsonCompuestaTercio());
+											}else
 												System.out.println("El valor de n debe ser par");
+										}while(Integer.parseInt(n)%2!=0);
+									}else if(Integer.parseInt(opcion)==4){												
+											String n;
+											do{
+												System.out.print("Ingresa la cantidad de puntos a evaluar: ");
+												n = leer.nextLine();
+												if(Integer.parseInt(n)%3==0){											
+													obj.setn(Integer.parseInt(n)+1);
+													double[] aux = obj.valorx(Double.parseDouble(x0),Double.parseDouble(xn),Integer.parseInt(opcion));
+													aux[0] = Double.parseDouble(x0);
+													aux[aux.length-1] = Double.parseDouble(xn);
+													obj.setx(aux);
+													double[] auxfx = obj.valorfx();
+													obj.setfx(auxfx);
+													ImprimirTabla(obj.getx(),obj.getfx(),obj.getn()-1,obj.SimpsonCompuestaOctavo());
+											
+												}else
+													System.out.println("El valor de n debe multiplo de 3");
+											}while(Integer.parseInt(n)%3!=0);
 									}
 
 					}
--- simpson/Simpson.java
+++ simpson/Simpson.java
@@ -6,7 +6,7 @@ class Simpson{
 				public double[] getx(){
 								return this.x;
 				}
-				public double[] fx(){
+				public double[] getfx(){
 								return this.fx;
 				}
 				public void setx(double[] x){
@@ -15,15 +15,25 @@ class Simpson{
 				public void setn(int n){
 								this.n=n;
 				}
+				public int getn(){
+							return this.n;
+				}
 				public void setfx(double[] fx){
 								this.fx=fx;
 				}
-
+				public double geth(){
+						return this.h;
+				}
+				public void seth(double h){
+								this.h=h;
+				}
 				public double polinomio(double x){
 						return Math.pow(x,2)+5*x-3;
+						//return Math.exp(-Math.pow(x,2));
+//						return (1.25*Math.sin(x/2)) + 3;
 				}
 				public void valorh(double x0,double xn){
-						h =  (xn-x0)/n;
+						h = (xn-x0)/(n-1);
 				}
 				public double[] valorx(double x0,double xn,int index){
 						double[] aux = new double[n];
@@ -33,10 +43,10 @@ class Simpson{
 							 	h = (xn-x0)/3;
 								aux[1]=(x0+h);
 								aux[2]=x0+(2*h);
-						}else if(index ==3 ){
+						}else if(index ==3 || index ==4){
 							valorh(x0,xn);
 
-							for(int i=1;i<n;i++){	
+							for(int i=1;i<n;i++){												
 									aux[i]=x0+i*h;
 							}
 						}
@@ -49,17 +59,31 @@ class Simpson{
 						}
 						return aux;
 				}
-				public double SimpsonCompuesta(){
+				public double SimpsonCompuestaTercio(){
 						double suma=0,suma2=0;
-						for(int i=0;i<(fx.length-2)/2;i++){
-								suma+=fx[(2*i)+1];
+						for(int i=1;i<=n-1;i+=2){							
+									suma+=fx[i];								
+								//System.out.println(suma);
 						}
 
-						for(int i=0;i<(fx.length-2)/2;i++){
-							suma2+=fx[2*i];
+						for(int i=2;i<n-2;i+=2){
+							suma2+=fx[i];
+							//System.out.println(suma2);
 						}
-
+						
+						//System.out.println(h+":"+n);
 						return (h/3)*(fx[0]+fx[fx.length-1]+(4*suma)+(2*suma2));
+				}
+				public double SimpsonCompuestaOctavo(){
+						double suma=0,suma2=0;
+							for(int i=1;i<=n-2;i+=3){	
+									suma+=(fx[i]+fx[i+1]);
+							}
+							for(int i=3;i<n-3;i+=3){
+									suma2+=fx[i];
+							}
+							//System.out.println(3*h/8+"["+fx[0]+"+"+fx[fx.length-1]+"+3("+suma+")2("+suma2+")]");
+					return ((3*h)/8)*(fx[0]+fx[fx.length-1]+(3*suma)+(2*suma2));
 				}
 				public double SimpsonTercio(){
 						double h = x[1]-x[0];
